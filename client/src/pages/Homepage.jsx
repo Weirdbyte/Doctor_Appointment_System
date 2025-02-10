06cdbf914b944c511c87d08a4d41ed093fd70e16
Sushant Nain <sushantnain0123@gmail.com>
2025-02-10 18:14:55 +0530

    Authorization complete

--- client/src/pages/Homepage.jsx
+++ client/src/pages/Homepage.jsx
@@ -1,6 +1,21 @@
-import React from 'react'
+import React, { useEffect } from 'react';
+
+import axios from 'axios';
 
 const Homepage = () => {
+
+  const getUserData = async()=>{
+    try {
+      const res = await axios.post("http://localhost:8080/api/v1/user/getUserData",{},{headers:{Authorization:"Bearer " + localStorage.getItem('token')}});
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
+  useEffect(()=>{
+    getUserData();
+  }, []);
+
   return (
     <div>
         <h1>Welcome to Home Page</h1>
--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -1,16 +1,18 @@
 const express = require("express");
-const {loginController,registerController} = require("../controllers/userCtrl");
+const {loginController,registerController, authController} = require("../controllers/userCtrl");
+const authMiddleware = require("../middlewares/authMiddleware");
 
 const router = express.Router();
  
 
 //routes----
 //LOGIN | POST
-
 router.post("/login" , loginController);
 
 //register | POST
-
 router.post("/register" , registerController);
+
+// Auth || POST
+router.post('/getUserData', authMiddleware , authController);
 
 module.exports = router;
